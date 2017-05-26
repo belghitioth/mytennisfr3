8506a262a8f035e0a5673365a52d33ccb61bbda3
belghitioth <belghitioth@gmail.com>
2017-05-26 10:03:07 +0200

    correction bower

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,9 +23,6 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular/angular.js',
-      'bower_components/moment/moment.js',
-      'bower_components/fullcalendar/dist/fullcalendar.js',
-      'bower_components/angular-ui-calendar/src/calendar.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
