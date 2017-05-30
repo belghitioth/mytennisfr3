4baccd30d34988455a6544b2b895d1a0c2b075e1
belghitioth <belghitioth@gmail.com>
2017-05-30 16:28:29 +0200

    correction partenaires

--- app/scripts/controllers/partenaires.js
+++ app/scripts/controllers/partenaires.js
@@ -11,8 +11,9 @@ angular.module('mytennisfr2App')
    		var club_id="";
    				var ref_clubs = firebase.database().ref('/clubs/');
 			var obj_clubs = $firebaseObject(ref_clubs);
-      $scope.demande={};
+    
 			
+      $scope.partenaires={};
 			obj_clubs.$loaded().then(function() {
 		     	angular.forEach(obj_clubs, function(value, key) {
 		           	if(value.nom==nomClub){		       			
@@ -30,32 +31,13 @@ angular.module('mytennisfr2App')
       		var sexe=$scope.demande.sexe;
       		var birthday=$scope.demande.birthday;
 
-          console.log(club_id);
-
-
         var ref_adherents=firebase.database().ref('/clubs/'+club_id+'/adherents');
-        //    ref_adherents.once("value")
-        //   .then(function(snapshot) {
-        //     snapshot.forEach(function(childSnapshot) {
-              
-              
-        //         if(childSnapshot.val()['classement']==classement)
-        //         {
       
-        //           $scope.partenaires[childSnapshot.val()['cid']]=childSnapshot.val();
-        //         }
-        //       })
-        //     console.log($scope.partenaires);
-        //   });
-
-      		console.log("sexe",sexe);
-            console.log("birthday",typeof(birthday));
-              console.log(classement);
-            console.log("type :",typeof(classement))
+          
 	      var obj_adherents=$firebaseObject(ref_adherents);
 			 obj_adherents.$loaded().then(function(){
           angular.forEach(obj_adherents,function(value,key){
-            if ((value.classement==classement || classement===undefined) && (value.sexe==sexe || sexe===undefined) && (value.birthday==birthday || classement===undefined)){
+            if ((value.classement==classement || classement==undefined) && (value.sexe==sexe || sexe==undefined) && (value.birthday==birthday || birthday==undefined)){
                 $scope.partenaires[key]=value;
             }
           
