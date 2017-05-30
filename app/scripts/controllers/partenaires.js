a729af6242bfa456b2144e5509323e91aba600ad
belghitioth <belghitioth@gmail.com>
2017-05-30 16:11:10 +0200

    modif partenaires

--- app/scripts/controllers/partenaires.js
+++ app/scripts/controllers/partenaires.js
@@ -11,6 +11,7 @@ angular.module('mytennisfr2App')
    		var club_id="";
    				var ref_clubs = firebase.database().ref('/clubs/');
 			var obj_clubs = $firebaseObject(ref_clubs);
+      $scope.demande={};
 			
 			obj_clubs.$loaded().then(function() {
 		     	angular.forEach(obj_clubs, function(value, key) {
@@ -47,15 +48,23 @@ angular.module('mytennisfr2App')
         //     console.log($scope.partenaires);
         //   });
 
-      		
+      		console.log("sexe",sexe);
+            console.log("birthday",typeof(birthday));
+              console.log(classement);
+            console.log("type :",typeof(classement))
 	      var obj_adherents=$firebaseObject(ref_adherents);
 			 obj_adherents.$loaded().then(function(){
           angular.forEach(obj_adherents,function(value,key){
+            if ((value.classement==classement || classement===undefined) && (value.sexe==sexe || sexe===undefined) && (value.birthday==birthday || classement===undefined)){
+                $scope.partenaires[key]=value;
+            }
+          
+
             
-            if ((value.classement==classement || classement==undefined) && (value.sexe==sexe || sexe==undefined) && (value.birthday==birthday || classement==undefined)){
-              $scope.partenaires[key]=value;
-              console.log($scope.partenaires);
-            };
+            // if ((value.classement==classement || classement===undefined) && (value.sexe==sexe || sexe===undefined) && (value.birthday==birthday || classement===undefined)){
+            //   $scope.partenaires[key]=value;
+            //   console.log($scope.partenaires);
+            // };
 
             
           });
